be318ebdad92003d00a9b214f63b59f2a12b9f00
tatsuya yokokura <yokokura.tatsuya@gmail.com>
2021-11-15 19:54:10 +0900

    feat: routing

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Events</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <h1>Events For Good</h1>
